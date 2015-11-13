9ee2e982deb60e7e8051ea734cb39e56f3b80ecd
Anne LoVerso <aelv13@gmail.com>
2015-11-12 19:34:15 -0800

    Better makefile, LED library
    The makefile is beautiful.  There is a library for LEDs that
    includes an init and a blinking function that can blink at a
    specific frequency.  There were some struggles here.

--- os/comp.sh
+++ os/comp.sh
@@ -1,7 +1,5 @@
+make tools.o
+make led.o
 make kernel.o
-make test.o
-make tty.o
-
-arm-none-eabi-gcc -mcpu=arm1176jzf-s -fpic -ffreestanding -c boot.S -o boot.o
-arm-none-eabi-gcc -T linker.ld -o myos.elf -ffreestanding -O2 -nostdlib boot.o kernel.o test.o tty.o
+make myos.elf
 arm-none-eabi-objcopy myos.elf -O binary myos.bin
